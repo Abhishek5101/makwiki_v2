3044d3956715fabe6692fdafdfe6fb5f5d8efb0c
Abhishek5101 <miabhishekkulkarni@gmail.com>
2019-12-05 15:58:02 -0800

    tweaked settings for deployment

--- wiki/forms.py
+++ wiki/forms.py
@@ -5,3 +5,6 @@ from wiki.models import Page
 class PageForm(forms.ModelForm):
     """ Render and process a form based on the Page model. """
     model = Page
+    
+    title = forms.CharField(max_length=255)
+    description = forms.TextInput()
